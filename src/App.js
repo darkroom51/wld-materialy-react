b705905052b5e41bf5666e6ce464cea36ee1ca8a
Mateusz Choma <mateusz@amazingdesign.eu>
2018-01-11 14:53:16 +0100

    First component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import MyFirstComponent from './MyFirstComponent'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <MyFirstComponent/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/MyFirstComponent.js
@@ -0,0 +1,5 @@
+const MyFirstComponent = () => null
+
+// function component should return JSX or null
+
+export default MyFirstComponent
