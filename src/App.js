056e2e06e9efc1614f544fd92de198ff39aca35c
Mateusz Choma <mateusz@amazingdesign.eu>
2018-01-12 13:14:17 +0100

    material-ui example

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import React, {Component} from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
+import MenuItem from 'material-ui/MenuItem'
 
 import Counter from './Counter'
 
 class App extends Component {
+    state = {
+        isDrawerOpen: false,
+        menuItems: ['First drawer item', 'Second drawer item', 'Third drawer item']
+    }
+
+    drawerToggle = () => {
+        this.setState({
+            isDrawerOpen: !this.state.isDrawerOpen
+        })
+    }
+
     render() {
+        console.log('Render App!', this.state)
         return (
-            <div>
-                <Counter />
-            </div>
+            <MuiThemeProvider>
+                <div>
+                    <AppBar
+                        title="My First App"
+                        onLeftIconButtonClick={this.drawerToggle}
+                    />
+                    <Drawer
+                        docked={false}
+                        width={250}
+                        open={this.state.isDrawerOpen}
+                        onRequestChange={this.drawerToggle}
+                    >
+                        {/*SOMETHIN FROM DB FOR EXAMPLE*/}
+                        {
+                            this.state.menuItems
+                                .map((textOnMenu, index) => (
+                                    <MenuItem key={index}>
+                                        {textOnMenu}
+                                    </MenuItem>
+                                ))
+                        }
+                    </Drawer>
+
+                    <Counter />
+                </div>
+            </MuiThemeProvider>
         )
     }
 }
