784c87f1fce90e9e2b14650417b8b2c1551e87e4
Mateusz Choma <mateusz@amazingdesign.eu>
2018-01-15 13:02:43 +0100

    Routes generation from array

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AppBar from 'material-ui/AppBar'
-import Drawer from 'material-ui/Drawer'
-import MenuItem from 'material-ui/MenuItem'
 
-import Counter from './Counter'
-import DefaultPropsComponent from './DefaultPropsComponent'
+import SideBar from './SideBar'
+
+import routes from './routes'
 
 class App extends Component {
     state = {
-        isDrawerOpen: false,
-        menuItems: ['First drawer item', 'Second drawer item', 'Third drawer item']
+        isDrawerOpen: false
     }
 
     drawerToggle = () => {
@@ -20,39 +20,30 @@ class App extends Component {
     }
 
     render() {
-        console.log('Render App!', this.state)
         return (
             <MuiThemeProvider>
-                <div>
-                    <AppBar
-                        title="My First App"
-                        onLeftIconButtonClick={this.drawerToggle}
-                    />
-                    <Drawer
-                        docked={false}
-                        width={250}
-                        open={this.state.isDrawerOpen}
-                        onRequestChange={this.drawerToggle}
-                    >
-                        {/*SOMETHIN FROM DB FOR EXAMPLE*/}
+                <Router>
+                    <div>
+                        <AppBar
+                            title="My First App"
+                            onLeftIconButtonClick={this.drawerToggle}
+                        />
+                        <SideBar
+                            isDrawerOpen={this.state.isDrawerOpen}
+                            drawerToggle={this.drawerToggle}
+                        />
+
                         {
-                            this.state.menuItems
-                                .map((textOnMenu, index) => (
-                                    <MenuItem key={index}>
-                                        {textOnMenu}
-                                    </MenuItem>
-                                ))
+                            routes.map(route => (
+                                <Route
+                                    key={route.path}
+                                    path={route.path}
+                                    component={route.component}
+                                />
+                            ))
                         }
-                    </Drawer>
-
-                    <Counter />
-
-                    <DefaultPropsComponent/>
-                    <DefaultPropsComponent name="Mateusz"/>
-                    <DefaultPropsComponent name="Ola"/>
-                    <DefaultPropsComponent name="Ala"/>
-                    <DefaultPropsComponent/>
-                </div>
+                    </div>
+                </Router>
             </MuiThemeProvider>
         )
     }
