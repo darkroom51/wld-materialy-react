9a448f9ad4c21566a48ab34eed7cb4f4bc187797
darkroom51 <darkroom@wp.pl>
2018-01-17 14:03:44 +0100

    DELETE added

--- src/RestToDoList.js
+++ src/RestToDoList.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const databaseUrl = 'https://ad-snadbox.firebaseio.com/JFDDL3/restToDo/waldek/';
+const databaseUrl = 'https://ad-snadbox.firebaseio.com/JFDDL3/restToDo/waldek/'
 
 class RestToDoList extends React.Component {
     state = {
         list: null,
         newTaskName: ''
+    }
+
+    componentWillMount() {
+        this.getData()
     }
 
     getData (){
@@ -15,26 +19,22 @@ class RestToDoList extends React.Component {
 
     }
 
-    componentWillMount() {
-        this.getData()
-    }
+    deleteTask = (taskId) => {
+        fetch(
+            databaseUrl + 'list/'+ taskId +'/.json',
+            {method: 'DELETE'}
+        )
+            .then(()=>this.getData())
+}
 
     handlerInputChange = (e) => this.setState({newTaskName: e.target.value})
 
     handlerButtonClick = () => {
         const newTaskObj = {name: this.state.newTaskName}
 
-        fetch(
-            databaseUrl + 'list/.json',
-            {
-                method: 'POST',
-                body: JSON.stringify(newTaskObj),
-                //headers:{} w FIREBASE nie musza byc
-            }
-        )
-            .then(()=> {console.log('POST OK'); this.getData()}) // pobieramy dane ale w .then, bo musimy miec pewnosc, ze POST juz zakonczyl sie wykonywac
+        fetch(databaseUrl + 'list/.json', {method: 'POST', body: JSON.stringify(newTaskObj)}) //headers:{} w FIREBASE nie musza byc
+            .then(()=> {console.log('POST OK'); this.getData(); this.setState({newTaskName : ''}); }) // pobieramy dane ale w .then, bo musimy miec pewnosc, ze POST juz zakonczyl sie wykonywac; odswierza sie dlatego, ze zmieniamy state
             .catch((err)=> console.log('POST ERR'))
-
     }
 
 
@@ -56,7 +56,14 @@ class RestToDoList extends React.Component {
                 {
                     Object.entries(this.state.list || {})
                         .map(([key, val]) => (    // key jest lepszy jako uid, niz index tablicy, index lepszy niz nic
-                            <div key={key}>{val.name}</div>
+                            <div key={key}>
+                                {val.name}
+                                <button
+                                onClick={() => this.deleteTask(key)} // funkcja wewnetrzna w anonimorej funkcji strzalkowej, zeby funckja wewnetrza wywolywala sie tylko na klikniecie
+                                >
+                                    X
+                                </button>
+                            </div>
                         ))
                 }
             </div> /*musimy przerobic obiek na tablice, bo React nie moze przyjmowac obiektow jako children*/
