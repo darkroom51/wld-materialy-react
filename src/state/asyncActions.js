8ce1113d0f8128d43ac1dc11eb37f0c79f63cf84
darkroom51 <darkroom@wp.pl>
2018-01-25 09:47:22 +0100

    Redux Asyns Actions; loading example

--- src/state/asyncActions.js
+++ src/state/asyncActions.js
@@ -1,21 +1,39 @@
 const SET_DATA = 'asyncActions/SET_DATA'
+const FETCHING_STARTED = 'asyncActions/FETCHING_STARTED'
+const FETCHING_FINISHED = 'asyncActions/FETCHING_FINISHED'
+const FETCHING_FAILED = 'asyncActions/FETCHING_FAILED'
 
-//kreateor akcji wrzuca dane
+//kreator akcji wrzuca dane
 export const setData = data => ({
     type: SET_DATA,
     data: data
 })
 
+const fetchingStarted = () => ({type: FETCHING_STARTED})
+const fetchingFinished = () => ({type: FETCHING_FINISHED})
+const fetchingFailed = () => ({type: FETCHING_FAILED})
+
 // Async ACTION i.e. data fetch ---> data=dispatch(setData()) vs data = setData()
 //funkcja zwracajaca funkcje ---> const fun=()=>()=>{}
 export const fetchData = () => (dispatch, getState) => {
+    dispatch(fetchingStarted()) // pierwsza akcja
     fetch('https://randomuser.me/api')
-        .then (response => response.json())
-        .then (data => dispatch(setData(data.results[0])))
+        .then(response => response.json())
+        .then(data => {
+            dispatch(setData(data.results[0]))
+            dispatch(fetchingFinished())
+        })
+        .catch(err => dispatch(fetchingFailed()))
 }
 
 const initialState = {
-    randomUserData: null
+    randomUserData: null,
+    messageForUser: ''
+}
+
+const messages = {
+    started: 'Ladowanie...',
+    failed: 'Blad pobierania danych!'
 }
 
 export default (state = initialState, action) => {
@@ -24,6 +42,21 @@ export default (state = initialState, action) => {
             return {
                 ...state,
                 randomUserData: action.data
+            }
+        case FETCHING_STARTED:
+            return {
+                ...state,
+                messageForUser: messages.started
+            }
+        case FETCHING_FINISHED:
+            return {
+                ...state,
+                messageForUser: initialState.messageForUser // messageForUser: ''
+            }
+        case FETCHING_FAILED:
+            return {
+                ...state,
+                messageForUser: messages.failed
             }
         default:
             return state
